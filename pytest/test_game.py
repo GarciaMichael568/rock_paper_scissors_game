b1e740b3df2c29d932a0199fdea03b4980d0fc48
Michael Garcia <garciamichael.developer@gmail.com>
2022-01-01 14:10:30 -0700

    finished test

---
+++ pytest/test_game.py
@@ -0,0 +1,10 @@
+import rock_paper_scissors
+from sys import stderr, stdin, stdout
+import io
+
+def testGame(capsys, monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('r'))
+    rock_paper_scissors.play()
+   # monkeypatch.setattr('sys.stdin', io.StringIO('r'))
+    captured = capsys.readouterr()
+    assert captured.out == "Input choice: 'r' for rock, 'p' for paper, 's' for scissors.\n"
--- pytest/test_pytest.py
+++
@@ -1,9 +0,0 @@
-from rock_paper_scissors import play
-from sys import stderr, stdin, stdout
-import io
-
-def testGame(capsys, monkeypatch):
-    monkeypatch.setattr('sys.stdin', io.StringIO('r'))
-    play()
-    stdout , stderr = capsys.readouterr()
-    assert stdout == 'Tie buddy'
